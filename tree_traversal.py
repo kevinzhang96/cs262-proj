7ce5ec4e2ca1b92f0479fc0f48f65ae18921cc06
Kevin Zhang <kevin.zhang500@gmail.com>
2018-04-24 17:15:12 -0400

    some updates to tree script

--- tree_traversal.py
+++ tree_traversal.py
@@ -4,25 +4,54 @@ import hashlib
 
 # read big files in 64kb chunks so we don't hog memory
 BLOCKSIZE = 65536
+MAX_HASH = 2 ** 40
 # use sha-1 hashing
 hasher = hashlib.sha1()
 # store the directory's file structure. key/value = path/filename
 tree = {}
 
-def file_hash(filename):
+def get_file_hash(filename):
 	with open(filename, 'rb') as file:
 	    buf = file.read(BLOCKSIZE)
 	    while len(buf):
 	        hasher.update(buf)
 	        buf = file.read(BLOCKSIZE)
-	return hasher.hexdigest()
+	return hasher.hexdigest()[:10]
 
 for root, dirs, files in os.walk(".", topdown=False):
+	# take off leading ./
+	root = root[2:]
+	
+	# json for this folder's files
+	root_json = {}
+	
+	# combined hash of all files in this directory
+	root_hash = 0
+	
+	# iterate through all files and store hashes + paths
 	for name in files:
 		path = os.path.join(root, name)
-		tree[path] = file_hash(os.path.join(root, name))
+		file_hash = get_file_hash(path)
+		root_hash += int(file_hash, 16)
+		root_hash %= MAX_HASH
+		root_json[name] = {
+			"path": path, 
+			"hash": file_hash
+		}
+	
+	# add hashes of all folders here to this directory's hash
 	for name in dirs:
-		tree[os.path.join(root, name)] = 'directory'
+		root_hash += int(tree[name]['hash'], 16)
+		root_hash %= MAX_HASH
+	
+	# insert this directory's hash and contents
+	root_hash = hex(root_hash)
+	if root_hash[-1] == "L":
+		root_hash = root_hash[:-1]
+	tree[root] = {
+		'hash': root_hash[2:],
+		'contents': root_json
+	}
 
 # dump the directory tree into JSON file
 with open('tree.json', 'w') as file:
