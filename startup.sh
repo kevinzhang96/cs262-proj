bc305fa0809c0e692d16d94cf606fa1eb1fb8ab9
Kevin Zhang <kevin.zhang500@gmail.com>
2018-05-04 02:28:09 -0400

    fix to startup script

--- startup.sh
+++ startup.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
-export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
-echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+if ! [-d backup ]; then
+    export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+    echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+fi
 
 sudo apt-get update
 sudo apt-get install -y gcsfuse
 sudo apt-get install -y python-pip
 
-gsutil mb -l us-east1 gs://$USER-$HOSTNAME
-mkdir backup; gcsfuse $USER-$HOSTNAME backup
+if ! [-d backup ]; then
+    gsutil mb -l us-east1 gs://$USER-$HOSTNAME
+    mkdir backup; gcsfuse $USER-$HOSTNAME backup
+fi
 
 gsutil cp -r gs://$USER-backup/* .
 cd ftp; python server.py
