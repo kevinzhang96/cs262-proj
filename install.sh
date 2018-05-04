89cd556238aadbc3b04a8275acb7cf908613cb17
Kevin Zhang <kevin.zhang500@gmail.com>
2018-05-04 00:32:27 -0400

    finally figured out SSH keys

--- install.sh
+++ install.sh
@@ -4,15 +4,20 @@ if [ $EUID != 0 ]; then
     exit $?
 fi
 
-# check to make sure python is installed
+USERNAME=$(who am i | awk '{print $1}')
+
+################################################################################
+### Package and Library Installs =========================================== ###
+################################################################################
+
+### Python ================================================================= ###
 if ! [[ $(which python) ]]; then
   echo "Python is not installed; try again after you've installed it!"
   exit
 fi
 
+### Pip ==================================================================== ###
 PIP_INSTALLED=$(which pip)
-
-# check to make sure pip is installed
 if ! [[ $PIP_INSTALLED ]]; then
   echo "Installing pip (the Python package manager)..."
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -30,41 +35,50 @@ else
   sudo pip install --upgrade pip &> /dev/null
 fi
 
-# install google cloud sdk
+### GCloud ================================================================= ###
 if ! [ -d ".sdk" ]; then
   echo "Installing the Google Cloud SDK..."
   curl https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-198.0.0-darwin-x86_64.tar.gz -o sdk.tar.gz
   gunzip -c sdk.tar.gz | tar xopf - &> /dev/null
-  sudo rm -rf sdk.tar.gz; sudo mv google-cloud-sdk .sdk
+  rm -rf sdk.tar.gz; mv google-cloud-sdk .sdk
+  chown -R $USERNAME .sdk
 fi
 
-# login to google cloud now
+################################################################################
+### Google Cloud Setup ===================================================== ###
+################################################################################
+
+### Login ================================================================== ###
 echo "Done! Logging in to Google Cloud now..."
 echo ""
 echo "-------------------------------------------------------------------------"
 echo ""
-
 .sdk/bin/gcloud config configurations activate default &> /dev/null
 .sdk/bin/gcloud auth login &> /dev/null
 
-if [ $? -eq 1 ]; then
-  echo "Google Cloud login failed.  Reverting installation..."
-  if ! [[ $PIP_INSTALLED ]]; then
-    for i in $( pip freeze ); do sudo pip uninstall -y $i &> /dev/null; done
-    sudo python -m pip uninstall -y pip setuptools &> /dev/null
-  fi
-
-  sudo rm -rf .sdk
-  sudo rm -rf ~/.config/gcloud
-  exit
-fi
-
-# create project for backup instances
+### Project================================================================= ###
 echo "Login complete! Creating backup project now..."
-.sdk/bin/gcloud projects create $USER-automatic-backup --quiet &> /dev/null
-.sdk/bin/gcloud config set project $USER-automatic-backup --quiet &> /dev/null
+.sdk/bin/gcloud projects create $USERNAME-automatic-backup --quiet &> /dev/null
+.sdk/bin/gcloud config set project $USERNAME-automatic-backup --quiet &> /dev/null
 .sdk/bin/gcloud services enable compute.googleapis.com --quiet &> /dev/null
 
-# begin creating instances on google cloud
+### Instances ============================================================== ###
 echo "Project creation complete! Initializing instances now..."
-.sdk/bin/gcloud compute instances create backup-instance1 --zone us-east1-b --machine-type f1-micro --quiet &> /dev/null
+.sdk/bin/gcloud compute instances create backup-instance1 \
+  --metadata-from-file startup-script=server.sh \
+  --zone us-east1-b \
+  --machine-type f1-micro \
+  --image-family=ubuntu-1604-lts \
+  --image-project=ubuntu-os-cloud \
+  --scopes=storage-full \
+  --tags=http-server,https-server \
+  --quiet &> /dev/null
+
+### SSH Keys =============================================================== ###
+echo "Instance creation complete! Adding SSH keys now..."
+if ! [ -f ~/.ssh/google_compute_engine.pub ]; then
+  ssh-keygen -t rsa -f $HOME/.ssh/google_compute_engine -N ''
+fi
+echo $USERNAME:$(cat ~/.ssh/google_compute_engine.pub) > ~/.ssh/google_compute_engine.txt
+gcloud compute project-info add-metadata --metadata-from-file ssh-keys=$HOME/.ssh/google_compute_engine.txt
+rm ~/.ssh/google_compute_engine.txt
