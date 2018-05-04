3150acb97f3cfdd0469355d37513b8227488604d
Kevin Zhang <kevin.zhang500@gmail.com>
2018-05-04 02:34:53 -0400

    more bugfixes to startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-if ! [-d backup ]; then
+if ! [ -d backup ]; then
     export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
     echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -9,10 +9,10 @@ sudo apt-get update
 sudo apt-get install -y gcsfuse
 sudo apt-get install -y python-pip
 
-if ! [-d backup ]; then
+if ! [ -d backup ]; then
     gsutil mb -l us-east1 gs://$USER-$HOSTNAME
     mkdir backup; gcsfuse $USER-$HOSTNAME backup
 fi
 
 gsutil cp -r gs://$USER-backup/* .
-cd ftp; python server.py
+cd ftp; sudo python server.py
