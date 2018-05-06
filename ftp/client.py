a6c6ad1d639f7084b3fb8122c1cd11b947fb2f75
Tomoya Hasegawa <tomoya.hasegawa@gmail.com>
2018-05-05 22:26:18 -0400

    exponential backoff on server connections

--- ftp/client.py
+++ ftp/client.py
@@ -1,5 +1,6 @@
 from crawler import Crawler
 from assistant import SFTPAssistant
+import time
 
 # all of these should be moved elsewhere
 NUM_REPLICAS = 1
@@ -10,12 +11,17 @@ SSH_PATH = "/Users/tomoya/.ssh/gcloud"
 REMOTE_PATH = "/home/tomoya/backup"
 LOCAL_PATH = "/Users/tomoya/coursework/CS262/cs262-proj/ftp"
 USER_NAME = "tomoya"
+TIMEOUT = 60
 
 for replica in range(NUM_REPLICAS):
 	sftp = SFTPAssistant(LOCAL_PATH, SSH_PATH)
-	if not sftp.connect(REPLICA_IPS[replica], REMOTE_PATH, USER_NAME):
-		print "Couldn't connect"
-		# handle connection failure with exponential backoff + timeout + server reboot etc
+	start_time = time.time()
+	n = 1
+	while not sftp.connect(REPLICA_IPS[replica], REMOTE_PATH, USER_NAME):
+		if time.time() >= start_time + TIMEOUT:
+			raise Exception("Server connection timed out.")
+		time.sleep(2**n)
+		n += 1
 
 	# crawl through LOCAL_PATH and recursively transfer all files/directories
 	sftp.upload_all()
